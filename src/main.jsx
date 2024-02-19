1f39569f86840a91fa31042de1d0757b803d1f3f
Maria Fernanda <fernandaantunesw@gmail.com>
2024-02-19 23:28:38 +0000

    change made

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { RentalProvider } from './components/RentalContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <RentalProvider>
     <App />
-    </RentalProvider>
     </Router>
   </React.StrictMode>,
 )
